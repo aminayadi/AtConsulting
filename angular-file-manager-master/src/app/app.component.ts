6295c52bb0eadbd2ec15ac005cfe701b904eb7bb
hatembellaaj <gitlab-mdsoft@mdsoft-int.com>
2021-05-05 23:41:39 +0100

    adding import dependencies

--- angular-file-manager-master/src/app/app.component.ts
+++ angular-file-manager-master/src/app/app.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 import { FileElement } from './file-explorer/model/element';
 import { Observable } from 'rxjs/Observable';
 import { FileService } from './service/file.service';
-
+import { HttpClient } from '@angular/common/http';
+import { User } from './user';
+import { MsalService } from '@azure/msal-angular';
+import { AlertsService } from './alerts.service';
+import { Client } from '@microsoft/microsoft-graph-client';
+import * as MicrosoftGraph from '@microsoft/microsoft-graph-types';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,13 +16,45 @@ import { FileService } from './service/file.service';
 export class AppComponent {
   public fileElements: Observable<FileElement[]>;
 
-  constructor(public fileService: FileService) {}
+  constructor(public fileService: FileService,
+    private _httpClient: HttpClient,
+    private msalService: MsalService,
+    private alertsService: AlertsService,
+    private http: HttpClient) 
+  {
+  /*  this.authenticated = this.msalService.getAccount() != null;
+    this.getUser().then((user) => {this.user = user});
+*/
+  }
 
   currentRoot: FileElement;
   currentPath: string;
   canNavigateUp = false;
+  public authenticated: boolean;
+  public user: User;
+  public token: string;
+  public dName:string;
+  //private http: HttpClient;
+
+  private readonly  URL = 'http://localhost:8082/api/atconsulting/service';
 
   ngOnInit() {
+
+/*
+    const headers = { 'Authorization': 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImF1dGgiOiJST0xFX0FETUlOLFJPTEVfVVNFUiIsImV4cCI6MTYxODMwNjY2Nn0.hokcCGCZGPMXcpTup6gm4hihP7uMVYnyxH8NsoAN5SOWvR1tD9TTXXOhSZB8u1RHkq8OSBnD_b2lkZBefMQOFQ',
+    'Content-Type': 'application/json' };
+const body = { bearer_token: this.token };
+console.log("coucou je suis là : execute post backend ------------------")
+return this.http.post<any>(this.URL, body, { headers }).subscribe(data => {
+this.dName = data;
+this.alertsService.addSuccess('Events from Graph', JSON.stringify(data, null, 9))
+//console.log("-------AYA Add----------- :"+ data.JSON.stringify.value);
+
+});*/
+
+
+
+
     const folderA = this.fileService.add({ name: 'Folder A', isFolder: true, parent: 'root' });
     this.fileService.add({ name: 'Folder B', isFolder: true, parent: 'root' });
     this.fileService.add({ name: 'Folder C', isFolder: true, parent: folderA.id });
@@ -83,4 +120,70 @@ export class AppComponent {
     p = split.join('/');
     return p;
   }
+/*
+  private async getUser(): Promise<User> {
+    if (!this.authenticated) return null;
+
+    let graphClient = Client.init({
+      // Initialize the Graph client with an auth
+      // provider that requests the token from the
+      // auth service
+      authProvider: async(done) => {
+        let token = await this.getAccessToken()
+          .catch((reason) => {
+            done(reason, null);
+          });
+
+        if (token)
+        {
+          console.log("TOKEN = " + token);
+          this.token = token ;
+          let results = this.ngOnInit();
+
+
+
+          done(null, token);
+        } else {
+          done("Could not get an access token", null);
+        }
+      }
+    });
+
+    // Get the user from Graph (GET /me)
+    let graphUser: MicrosoftGraph.User = await graphClient
+      .api('/me')
+      .select('displayName,mail,mailboxSettings,userPrincipalName')
+      .get();
+
+    let user = new User();
+    user.displayName = graphUser.displayName;
+    // Prefer the mail property, but fall back to userPrincipalName
+    user.email = graphUser.mail || graphUser.userPrincipalName;
+    user.timeZone = graphUser.mailboxSettings.timeZone;
+
+    // Use default avatar
+    user.avatar = '/assets/no-profile-photo.png';
+
+    return user;
+  }
+  // </getUserSnippet>
+
+/*
+    // Silently request an access token
+    async getAccessToken(): Promise<string> {
+      let result = await this.msalService.acquireTokenSilent(OAuthSettings)
+        .catch((reason) => {
+          this.alertsService.addError('Get token failed', JSON.stringify(reason, null, 2));
+        });
+  
+      if (result) {
+        return result.accessToken;
+      }
+  
+      // Couldn't get a token
+      this.authenticated = false;
+      return null;
+    }
+*/
+
 }
