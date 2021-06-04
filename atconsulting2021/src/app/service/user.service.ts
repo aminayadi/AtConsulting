771618b10752a9562547eac8370b80acf8fba503
aminayadi <amin.ayadi@esprit.tn>
2021-06-04 17:12:59 +0100

    create oki

--- atconsulting2021/src/app/service/user.service.ts
+++ atconsulting2021/src/app/service/user.service.ts
@@ -49,7 +49,48 @@ export class UserService {
 
 
   saveUser(user:User) {
-    this.userList.push(user);
+
+   // this.userList.push(user);
+ const token=this.auth_token;
+     console.log(token);
+    this.headers = new HttpHeaders().set('Authorization', 'Bearer ' + token)
+                                   .set("Content-Type", "application/json");
+    const headers = { 'Authorization': 'Bearer ', token };
+
+    const body = JSON.stringify({
+      "client": {
+        "adress": user.adress,
+        "contact": user.contact,
+        "email": user.email,
+        "id": 0,
+        "name": user.name,
+        "phone": user.phone,
+        "type": user.type
+      },
+      "connection": {
+        "bearer_token": "EwB4A8l6BAAU6k7+XVQzkGyMv7VHB/h4cHbJYRAAAQXoJNdnp3xdvZR09okeVnRnghvSYdRpkc7ZgdBJVOgE/kChq9PiHL7IyVEZrwVGj3048iudl1MbeUu82WhZR6gzDI1VWG5t8y37mJat5gtApAdHwxub74/mBi/uCHRgFiP/gRxhHuHm8BzFjlMk4c/hbwIppSRrU00+Jo69v1jgBoyfbSyeBupdcyDZbXTqJ0620xO9gyQO/JVPgBmRQrT57atChBqZ/oi325W8qEeRdg4u45zEo/VlOZx94ylErc4K3N9IytYH5v4s/JPC1l7yW9PRTHTk8yEmfVgwpMy84vdYdWyyWQtXgiXIs49Q4xELo19IsIT7NEWekgl4cAoDZgAACCb+rqmXDgfFSAIKVJIfZYGhuVB9E5ID4/RmvcHjxsi1zYj3kG6sAgA/NrN6QPXMV1RSlqqeabCnxAhrNZ3U+9IOkFp1kPxtlCwq/aV7gboADsNTkdIwv0doUe15rk+BapsBVg4LufAD+M8i2qd3j5s4QReWgPwVDIdGKLHRlSkpZfXaBZH+LPLNGz97b4+AVqfCm9glIaD7/dw0Cf7+z1vypVvNq5sK69+okV0QGdGt5M4cNXmRBbt+FWWuZM+KxVCz4ihCIywoY6XjUjsik2GATnjsT5EdrovpNf1xoR7CB3LnP1OS1eckUQx+2VukpCOyyh3adwmh1EN8YIMSn/BWJ5ot8oFpyImOF9GxRBKzW5plM8vtB9/MLMEbksDYLZEEIk61uxSOorc/GJ9WATp381mb3ACCgxV4KwxjNNZ7fhdG/fSyw8LJplDiadxIIUBv3G9qIc5ea9k640MOIoIqUWL0U3ljI0I6Oxim6YjzhkPUwgiV55XXgRAUGQSGuYmaGGcJ/v45975aDbP/jZAmPyeHW73L5SqN0T+ZWy1S8H0Kp6J0wdFDBhrxe7Gsa25/NvjxP+QBubeVGaKCceSgiMDiXMYtCSoz+t9GNOkV709dcUyVarFwuvGdLOP9hty33PzfUDNiNdSiI5ZxiVmvRqP42wfGJuuBjPYDjZ0EtDZD9WJEkvhr7QSUKy1fsdQNf5Ss9yvq7x/Vtr/gGm5jYDSQwXS/BDv+Tce2Yx3PVA/oNDJxQ1K+TpxoXO7y5tyRL58+vIzTv7TuHYYFfB3Hl44C",
+        "idFolder": "string"
+      }
+    });
+    this.http.post<any>('http://localhost:8082/api/ajouterClient', body, {headers: this.headers}).subscribe(data => {
+
+                          //this.userList=this.ELEMENT_DATA;
+
+                          console.log("server connection ok + response : " + data);
+
+  })
+
+
+       /*
+       .post("http://localhost:8082/api/ajouterClient", JSON.stringify(user), {headers: this.headers},)
+       .toPromise()
+       .then(res => {   // this.userList = response.map(item => new User(item));
+       console.log("server connection ok + response : " + res);})
+       .catch(error => console.log('error', error));
+
+*/
+
+
   }
 
   updateUser(user:User){
