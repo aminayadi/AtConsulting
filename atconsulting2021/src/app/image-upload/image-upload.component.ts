fca454e0dfbc3f1e0dc4165627bd216ba54a9d1b
aminayadi <amin.ayadi@esprit.tn>
2021-06-29 12:33:50 +0100

    upload photo oki

--- atconsulting2021/src/app/image-upload/image-upload.component.ts
+++ atconsulting2021/src/app/image-upload/image-upload.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, Input, OnInit, SystemJsNgModuleLoader } from '@angular/core';
+import { environment } from 'src/environments/environment';
 import { ImageService } from './image.service';
 class ImageSnippet {
   pending: boolean = false;
@@ -10,21 +12,15 @@ class ImageSnippet {
   templateUrl: './image-upload.component.html',
   styleUrls: ['./image-upload.component.css']
 })
-export class ImageUploadComponent  implements OnInit {
+export class ImageUploadComponent {
+  @Input() token: string;
   selectedFile: ImageSnippet;
 
-  constructor(private imageService: ImageService){}
-  ngOnInit(): void {}
-  private onSuccess() {
-    this.selectedFile.pending = false;
-    this.selectedFile.status = 'ok';
-  }
+  private baseUrl = environment.host;
+  private auth_token="eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImF1dGgiOiJST0xFX0FETUlOLFJPTEVfVVNFUiIsImV4cCI6MTYyMzc0OTIyM30.VCAXXSmFBeyIpaMz4xDQYXIErCBl5UxDxyxvz2qy6Lrzr5vKKLiefE09VFMfV6tHWmGRxcs8gX5pccP3RCp0tQ";
+   headers!: HttpHeaders;
+  constructor(private http: HttpClient, private imageService: ImageService){}
 
-  private onError() {
-    this.selectedFile.pending = false;
-    this.selectedFile.status = 'fail';
-    this.selectedFile.src = '';
-  }
 
   processFile(imageInput: any) {
     const file: File = imageInput.files[0];
@@ -33,17 +29,40 @@ export class ImageUploadComponent  implements OnInit {
     reader.addEventListener('load', (event: any) => {
 
       this.selectedFile = new ImageSnippet(event.target.result, file);
+console.log("Binary Image 64 : " + this.selectedFile.src.toString() );
+console.log("fileName  : " + this.selectedFile.file.name);
+console.log("TOKEN BLOC  : " + this.token);
 
-      this.selectedFile.pending = true;
-      this.imageService.uploadImage(this.selectedFile.file).subscribe(
-        (res) => {
-          this.onSuccess();
-        },
-        (err) => {
-          this.onError();
-        })
+
+this.headers = new HttpHeaders().set('Authorization', 'Bearer ' + this.auth_token)
+.set("Content-Type", "application/json");
+
+ 
+ const body = JSON.stringify({
+
+  "connection": {
+    "bearer_token": this.token,
+    "idFolder": "root"
+  },
+  "pName": this.selectedFile.file.name,
+  "photo": this.selectedFile.src.substring(this.selectedFile.src.lastIndexOf(",") + 1)
+ });
+ this.http.put<any>('http://localhost:8082/api/ajouterPhoto', body, {headers: this.headers}).subscribe(data => {
+ 
+                       
+ 
+                       console.log("server connection ok + response : " + data);
+                      
+ });
+
+
+
     });
 
     reader.readAsDataURL(file);
   }
+}
+
+function simpleAlertBox() {
+  throw new Error('Function not implemented.');
 }
